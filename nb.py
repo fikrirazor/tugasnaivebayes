d055246ecf2aba5126dc6a368915ab0db080331d
fikrirazor <fikrirazor@outlook.co.id>
2019-02-28 21:26:19 +0700

    Update nb.py

--- nb.py
+++ nb.py
@@ -7,19 +7,32 @@ Created on Thu Feb 28 19:11:44 2019
 
 import pandas as pd
 
-file = r'data/TrainsetTugas1ML.csv'
-df = pd.read_csv(file)
-print(df)
+Train = r'data/TrainsetTugas1ML.csv'
+dn = pd.read_csv(Train)
+print(dn)
 
-peluangincome= df.income.value_counts()/df.income.count()
-#usia=df.groupby("age").income.value_counts()
-usia=df.groupby('age')['income'].value_counts()/df.groupby('age')['income'].count()
-#workclass=df.groupby("workclass").income.value_counts()
-workclass=df.groupby("workclass")['income'].value_counts()/df.groupby('workclass')['income'].count()
-education=df.groupby("education").income.value_counts()
-maritalstatus=df.groupby("marital-status").income.value_counts()
-occupation=df.groupby("occupation").income.value_counts()
-relationship=df.groupby("relationship").income.value_counts()
-hoursperweek=df.groupby("hours-per-week").income.value_counts()
+peluangincome= dn.income.value_counts()/dn.income.count()
+#usia=dn.groupby("age").income.value_counts()
+us=dn.groupby('age')['income']
+usia=us.value_counts()/us.count()
+#workclass=dn.groupby("workclass").income.value_counts()
+wr=dn.groupby("workclass")['income']
+workclass=wr.value_counts()/wr.count()
+#education=dn.groupby("education").income.value_counts()
+ed=dn.groupby("education")['income']
+education=ed.value_counts()/ed.count()
+#maritalstatus=dn.groupby("marital-status").income.value_counts()
+ms=dn.groupby("marital-status")['income']
+education=ms.value_counts()/ms.count()
+#occupation=dn.groupby("occupation").income.value_counts()
+oc=dn.groupby("occupation")['income']
+occupation=oc.value_counts()/oc.count()
+#relationship=dn.groupby("relationship").income.value_counts()
+re=dn.groupby("relationship")['income']
+relationship=re.value_counts()/re.count()
+#hoursperweek=dn.groupby("hours-per-week").income.value_counts()
+ho=dn.groupby("hours-per-week")['income']
+hoursperweek=ho.value_counts()/ho.count()
 
-#frequensi age
+Test = r'data/TestsetTugas1ML.csv'
+dt = pd.read_csv(Test)
